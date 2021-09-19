3e8e3d8e7ff0a6a007a5a3532af6302d5968e9fa
Damian Szklarczyk <damian.szk@gmail.com>
2021-09-19 14:25:45 +0200

    test: Removed newUri

--- R/r_utils.R
+++ R/r_utils.R
@@ -16,18 +16,18 @@ postFormSmart <- function(uri, ..., .params = list(), .opts = curlOptions(url = 
              .contentEncodeFun = .contentEncodeFun)
   
     
-    suppressWarnings( if(grepl("The document has moved", res)){
-    
-      begin <- regexpr("href",res)+6
-      mys2=substr(res, begin, 10000000)
-      end <- regexpr('"',mys2)-1
-      uriNew = substr(mys2, 1, end)
-      
-      res=postForm(uriNew, ..., .params = .params, .opts = .opts,
-               curl = curl, style = style,
-               .encoding = .encoding, binary = binary, .checkParams = .checkParams,
-               .contentEncodeFun = .contentEncodeFun)
-    } )
+    # suppressWarnings( if(grepl("The document has moved", res)){
+    # 
+    #   begin <- regexpr("href",res)+6
+    #   mys2=substr(res, begin, 10000000)
+    #   end <- regexpr('"',mys2)-1
+    #   uriNew = substr(mys2, 1, end)
+    #   
+    #   res=postForm(uriNew, ..., .params = .params, .opts = .opts,
+    #            curl = curl, style = style,
+    #            .encoding = .encoding, binary = binary, .checkParams = .checkParams,
+    #            .contentEncodeFun = .contentEncodeFun)
+    # } )
     
     return(res)
   
