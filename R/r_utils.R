4317a1dda30e1898c36aaffebb180aca93a648bf
Damian Szklarczyk <damian@Damians-MacBook-Air.local>
2026-04-10 15:41:59 +0200

    Alias disambiguation improved.

--- R/r_utils.R
+++ R/r_utils.R
@@ -5,248 +5,196 @@
 #
 
 
-postFormSmart <- function(uri, .params = list(), .opts = list(), .ctype="text"){
-
-  res <- POST(url = uri, body = .params, config = .opts )
+postFormSmart <- function(uri, .params = list(), .opts = list(), .ctype = "text") {
+  res <- POST(url = uri, body = .params, config = .opts)
   data <- content(res, as = .ctype)
   return(data)
-
 }
 
-# postFormSmart <- function(uri, ..., .params = list(), .opts = curlOptions(url = uri),
-#                           curl = getCurlHandle(), style = 'HTTPPOST',
-#                           .encoding = integer(), binary = NA, .checkParams = TRUE,
-#                           .contentEncodeFun = curlEscape){
-# 
-#     res = postForm(uri, ..., .params = .params, .opts = .opts,
-#              curl = curl, style = style,
-#              .encoding = .encoding, binary = binary, .checkParams = .checkParams,
-#              .contentEncodeFun = .contentEncodeFun)
-#   
-# 
-#     
-#     # suppressWarnings( if(grepl("The document has moved", res)){
-#     # 
-#     #   begin <- regexpr("href",res)+6
-#     #   mys2=substr(res, begin, 10000000)
-#     #   end <- regexpr('"',mys2)-1
-#     #   uriNew = substr(mys2, 1, end)
-#     #   
-#     #   res=postForm(uriNew, ..., .params = .params, .opts = .opts,
-#     #            curl = curl, style = style,
-#     #            .encoding = .encoding, binary = binary, .checkParams = .checkParams,
-#     #            .contentEncodeFun = .contentEncodeFun)
-#     # } )
-#     
-#     return(res)
-#   
-# }
+
+coeffOfvar <- function(x) {
+  return(sd(x) / mean(x))
+}
 
 
-coeffOfvar <- function(x){
-  return(sd(x)/mean(x))
-} 
+PREFERRED_ALIAS_SOURCES <- c(
+  "BioMart_HUGO",
+  "Ensembl_HGNC",
+  "SGD_NAME",
+  "FlyBase_PRIMARY",
+  "TAIR_PRIMARY",
+  "SGD_PRIMARY",
+  "PomBase_NAME",
+  "Ensembl_SGD",
+  "Ensembl_TAIR",
+  "Ensembl_MGI",
+  "Ensembl_FlyBaseName_gene",
+  "Ensembl_flybase_gene_id",
+  "Ensembl_EntrezGene",
+  "Ensembl_WikiGene",
+  "Ensembl_HGNC_automatic_gene",
+  "Ensembl_MGI_automatic_gene",
+  "Ensembl_HGNC_curated_gene",
+  "RefSeq_gene",
+  "PomBase_ID",
+  "Ensembl_UniProt_GN",
+  "UniProt_GN_Name",
+  "KEGG_NAME",
+  "MaizeGDB_SYMBOL",
+  "WormBase_PRIMARY",
+  "FlyBase_SYNONYM",
+  "TAIR_SYNONYM",
+  "RefSeq",
+  "UniProt_GN_ORFNames",
+  "UniProt_ID"
+)
+
+
+prioritize_aliases_by_source <- function(aliasDf) {
+  if (nrow(aliasDf) == 0) {
+    return(aliasDf)
+  }
+
+  rank_lookup <- seq_along(PREFERRED_ALIAS_SOURCES)
+  names(rank_lookup) <- PREFERRED_ALIAS_SOURCES
+
+  aliasDf$alias_normalized <- toupper(iconv(aliasDf$alias, "WINDOWS-1252", "UTF-8"))
+  ambiguous <- duplicated(aliasDf$alias_normalized) | duplicated(aliasDf$alias_normalized, fromLast = TRUE)
+
+  if (!any(ambiguous)) {
+    return(subset(aliasDf, select = c("STRING_id", "alias")))
+  }
+
+  aliasDf$row_order <- seq_len(nrow(aliasDf))
+  aliasDf$first_alias_order <- match(aliasDf$alias_normalized, aliasDf$alias_normalized)
+
+  aliasDf_ambiguous <- aliasDf[ambiguous, ]
+  aliasDf_ambiguous$source_rank <- length(PREFERRED_ALIAS_SOURCES) + 100
+  aliasDf_ambiguous$source_rank[aliasDf_ambiguous$sources == "preferred_name"] <- 0
+  aliasDf_ambiguous$source_rank[aliasDf_ambiguous$sources == "STRING_id"] <- 1
+
+  ranked_sources <- rank_lookup[aliasDf_ambiguous$sources]
+  aliasDf_ambiguous$source_rank[!is.na(ranked_sources)] <- unname(ranked_sources[!is.na(ranked_sources)]) + 1
+
+  aliasDf_ambiguous <- aliasDf_ambiguous[order(aliasDf_ambiguous$alias_normalized, aliasDf_ambiguous$source_rank, aliasDf_ambiguous$row_order), ]
+  aliasDf_ambiguous <- aliasDf_ambiguous[!duplicated(aliasDf_ambiguous$alias_normalized), ]
+  aliasDf_ambiguous$source_rank <- NULL
+
+  aliasDf_unambiguous <- aliasDf[!ambiguous, ]
+  aliasDf_selected <- rbind(aliasDf_unambiguous, aliasDf_ambiguous)
+  aliasDf_selected <- aliasDf_selected[order(aliasDf_selected$first_alias_order), ]
+
+  return(subset(aliasDf_selected, select = c("STRING_id", "alias")))
+}
 
 
 # delete column in data frame
-delColDf <- function(df, colName){
-  if(colName %in% names(df)) return( df[ , -which(names(df) %in% c(colName))] )
-  else return(df)
+delColDf <- function(df, colName) {
+  if (colName %in% names(df)) {
+    return(df[, -which(names(df) %in% c(colName))])
+  } else {
+    return(df)
+  }
 }
 
 
 # download a file when it is not already present and also check the dimension of the file with the STRING server
-downloadAbsentFileSTRING <- function(urlStr, oD = tempdir()){
-  expectedSize=read.table(url(paste("http://string.uzh.ch/permanent_scripts/get_file_size.pl?file=", urlStr, sep="")), stringsAsFactors=FALSE, fill = TRUE)$V1
-  
-  fileName = tail(strsplit(urlStr, "/")[[1]], 1)
-  temp <- paste(oD,"/", fileName, sep="")
-  if(! file.exists(temp) || file.info(temp)$size==0 || file.info(temp)$size!=expectedSize) download.file(urlStr,temp)
-  if(file.info(temp)$size==0) {
+downloadAbsentFileSTRING <- function(urlStr, oD = tempdir()) {
+  expectedSize <- read.table(url(paste("http://string.uzh.ch/permanent_scripts/get_file_size.pl?file=", urlStr, sep = "")), stringsAsFactors = FALSE, fill = TRUE)$V1
+
+  fileName <- tail(strsplit(urlStr, "/")[[1]], 1)
+  temp <- paste(oD, "/", fileName, sep = "")
+  if (!file.exists(temp) || file.info(temp)$size == 0 || file.info(temp)$size != expectedSize) download.file(urlStr, temp)
+  if (file.info(temp)$size == 0) {
     unlink(temp)
-    temp=NULL
-    cat(paste("ERROR: failed to download ", fileName,".\nPlease check your internet connection and/or try again. " , 
-              "\nThen, if you still display this error message please contact us.",sep=""))
-  }  
+    temp <- NULL
+    cat(paste("ERROR: failed to download ", fileName, ".\nPlease check your internet connection and/or try again. ",
+      "\nThen, if you still display this error message please contact us.",
+      sep = ""
+    ))
+  }
   return(temp)
 }
 
 
 # download a file when it is not already present
-downloadAbsentFile <- function(urlStr, oD = tempdir()){
-  
-  fileName = tail(strsplit(urlStr, "/")[[1]], 1)
-  temp <- paste(oD,"/", fileName, sep="")
-  if(! file.exists(temp) || file.info(temp)$size==0) download.file(urlStr,temp)
-  if(file.info(temp)$size==0) {
+downloadAbsentFile <- function(urlStr, oD = tempdir()) {
+  fileName <- tail(strsplit(urlStr, "/")[[1]], 1)
+  temp <- paste(oD, "/", fileName, sep = "")
+  if (!file.exists(temp) || file.info(temp)$size == 0) download.file(urlStr, temp)
+  if (file.info(temp)$size == 0) {
     unlink(temp)
-    temp=NULL
-    cat(paste("ERROR: failed to download ", fileName,".\nPlease check your internet connection and/or try again. " , 
-              "\nThen, if you still display this error message please contact us.",sep=""))
-  }  
+    temp <- NULL
+    cat(paste("ERROR: failed to download ", fileName, ".\nPlease check your internet connection and/or try again. ",
+      "\nThen, if you still display this error message please contact us.",
+      sep = ""
+    ))
+  }
   return(temp)
 }
 
 
 
-# drop levels from vector
-# dropLevelsFV <- function(vect){
-#   newVect = rep(NA, length(vect))
-#   for(i in 1:length(vect)){newVect[i] = vect[i]}
-#   return(newVect)
-# } 
-
-
-# sort the vector and extract the values in between startPerc and endPerc 
-# extract_values <- function(vect, startPerc, endPerc){
-# 	vect = vect[!is.na(vect)]
-# 	indexA=floor((length(vect)* startPerc) + 1)
-# 	indexB=floor(length(vect)* endPerc)
-# 	vectToReturn = sort(vect)[indexA:indexB]
-# 	return(vectToReturn)
-# }
-
-
-# intersectAll <-function(...){
-#   vectors <- list(...)
-#   intersection = vectors[[1]]
-#   for(i in 1:length(vectors)){
-#     intersection = intersect(intersection, vectors[[i]])
-#   }
-#   return(intersection)
-# }
-
-
-## move column in the defined position
-# moveCol <- function(df, colName, new_position=1){
-# 	col_idx <- grep(colName, names(df))
-# 	posArr = 1:ncol(df)
-# 	posArr = append(posArr[-col_idx], col_idx, after=new_position-1)
-# 	df <- df[, posArr]
-# 	return(df)
-# }
-
-
-## move column in the position after colNameBefore
-# moveColAfter <- function(df, colName, colNameBefore){
-# 	new_position <- (grep(colNameBefore, names(df)))+1
-# 	return(moveCol(df,colName,new_position))
-# }
-
-
-
 # Merge preserving the original order if sort==FALSE (this doesn't happen in the original merge implementation)
-merge.with.order <- function(x,y, ..., sort = T)
-{
+merge.with.order <- function(x, y, ..., sort = T) {
   # this function works just like merge, only that it adds the option to return the merged data.frame ordered by x (1) or by y (2)
-  add.id.column.to.data <- function(DATA)
-  {
+  add.id.column.to.data <- function(DATA) {
     data.frame(DATA, id... = seq_len(nrow(DATA)))
   }
   # add.id.column.to.data(data.frame(x = rnorm(5), x2 = rnorm(5)))
-  order.by.id...and.remove.it <- function(DATA)
-  {
+  order.by.id...and.remove.it <- function(DATA) {
     # gets in a data.frame with the "id..." column.  Orders by it and returns it
-    if(!any(colnames(DATA)=="id...")) stop("The function order.by.id...and.remove.it only works with data.frame objects which includes the 'id...' order column")
-    
+    if (!any(colnames(DATA) == "id...")) stop("The function order.by.id...and.remove.it only works with data.frame objects which includes the 'id...' order column")
+
     ss_r <- order(DATA$id...)
     ss_c <- colnames(DATA) != "id..."
     DATA[ss_r, ss_c]
   }
-  
-  if(sort==F){ return(order.by.id...and.remove.it(merge(x=add.id.column.to.data(x),y=y,..., sort = FALSE)))
-  } else {return(merge(x=x,y=y,..., sort = sort))}
-  
+
+  if (sort == F) {
+    return(order.by.id...and.remove.it(merge(x = add.id.column.to.data(x), y = y, ..., sort = FALSE)))
+  } else {
+    return(merge(x = x, y = y, ..., sort = sort))
+  }
 }
 
 
 # mapping function (add the possibility to map using more than one column)
-multi_map_df <- function(dfToMap, dfMap, strColsFrom, strColFromDfMap, strColToDfMap, caseSensitive=FALSE){
-  
-  tempMatr = matrix(NA, length(strColsFrom), nrow(dfToMap))
-  for(i in 1:length(strColsFrom)){
-    if(!caseSensitive){ 
-      tempMatr[i,] = as.vector(dfToMap[,strColsFrom[i]])
-      dfToMap[,strColsFrom[i]] = toupper(iconv(dfToMap[,strColsFrom[i]], "WINDOWS-1252", "UTF-8")) 
+multi_map_df <- function(dfToMap, dfMap, strColsFrom, strColFromDfMap, strColToDfMap, caseSensitive = FALSE) {
+  tempMatr <- matrix(NA, length(strColsFrom), nrow(dfToMap))
+  for (i in 1:length(strColsFrom)) {
+    if (!caseSensitive) {
+      tempMatr[i, ] <- as.vector(dfToMap[, strColsFrom[i]])
+      dfToMap[, strColsFrom[i]] <- toupper(iconv(dfToMap[, strColsFrom[i]], "WINDOWS-1252", "UTF-8"))
     }
   }
-  if(!caseSensitive){ dfMap[,strColFromDfMap] = toupper(iconv(dfMap[,strColFromDfMap], "WINDOWS-1252", "UTF-8")) }
-  
-  dfMap2 = unique(subset(dfMap, select=c(strColFromDfMap, strColToDfMap)))
-  df2 = merge.with.order(dfToMap, dfMap2, by.x=strColsFrom[1], by.y=strColFromDfMap, all.x=TRUE, sort=FALSE)
-  if(length(strColsFrom) > 1){
-    for(i in 2:length(strColsFrom)){
-      dfna = delColDf(subset(df2, is.na(as.vector(df2[, strColToDfMap]))), strColToDfMap)
-      dfgood = subset(df2, !is.na(as.vector(df2[, strColToDfMap])))
-      df3 = merge.with.order(dfna, dfMap2, by.x=strColsFrom[i], by.y=strColFromDfMap, all.x=TRUE, sort=FALSE)
-      df2 = rbind(dfgood, df3)
-    }  
+  if (!caseSensitive) {
+    dfMap[, strColFromDfMap] <- toupper(iconv(dfMap[, strColFromDfMap], "WINDOWS-1252", "UTF-8"))
   }
-  
-  for(i in 1:length(strColsFrom)){
-    if(!caseSensitive && length(tempMatr[i,])==length(df2[,strColsFrom[i]])) df2[,strColsFrom[i]] = tempMatr[i,]
+
+  dfMap2 <- unique(subset(dfMap, select = c(strColFromDfMap, strColToDfMap)))
+  df2 <- merge.with.order(dfToMap, dfMap2, by.x = strColsFrom[1], by.y = strColFromDfMap, all.x = TRUE, sort = FALSE)
+  if (length(strColsFrom) > 1) {
+    for (i in 2:length(strColsFrom)) {
+      dfna <- delColDf(subset(df2, is.na(as.vector(df2[, strColToDfMap]))), strColToDfMap)
+      dfgood <- subset(df2, !is.na(as.vector(df2[, strColToDfMap])))
+      df3 <- merge.with.order(dfna, dfMap2, by.x = strColsFrom[i], by.y = strColFromDfMap, all.x = TRUE, sort = FALSE)
+      df2 <- rbind(dfgood, df3)
+    }
   }
-  
+
+  for (i in 1:length(strColsFrom)) {
+    if (!caseSensitive && length(tempMatr[i, ]) == length(df2[, strColsFrom[i]])) df2[, strColsFrom[i]] <- tempMatr[i, ]
+  }
+
   return(df2)
 }
 
 
 
-# sum n random numbers
-# sumRandom <- function(n){
-#   rand=0
-#   for(i in 1:n){
-#     rand=rand+sample(1:100,1,replace=TRUE)/100
-#   }
-#   return(rand)
-# }
-
-  
-# generate a vector contaning as elements sums of n random numbers 
-# sumRandomVect <- function(n, vLength){
-#   vRand = NULL
-#   for(i in 1:vLength){
-#     vRand = c(vRand, sumRandom(n))
-#   }
-#   return(vRand)
-# }
-
-
 ## Rename column data frame
-renameColDf <- function(df, colOldName, colNewName){
-	if(! (colOldName %in% names(df)) ) print(paste("ERROR: We cannot find ", colOldName, " in the data frame.", sep=""))
-  names(df)[ which( names( df ) == colOldName ) ] <- colNewName
-	return(df)
+renameColDf <- function(df, colOldName, colNewName) {
+  if (!(colOldName %in% names(df))) print(paste("ERROR: We cannot find ", colOldName, " in the data frame.", sep = ""))
+  names(df)[which(names(df) == colOldName)] <- colNewName
+  return(df)
 }
-
-
-# replace elements of inputVect with the corresponding element in replacementVect when it is not null
-# replace_non_null_elements <- function(inputVect, replacementVect){
-#   for(i in 1:length(inputVect)){
-#     if(!is.na(replacementVect[i]) && !is.null(replacementVect[i])) inputVect[i] = replacementVect[i]
-#   }
-#   return(inputVect)
-# }
-
-
-## round values of the specified columns
-# roundDf <- function(df, colNames, digits=3){
-# 	for( i in 1:length(colNames) ){
-# 		df[colNames[i]] = round(df[colNames[i]], digits)
-# 	}
-# 	return(df)
-# } 
-
-
-
-# Union all the vectors
-# unionAll <-function(...){
-#   vectors <- list(...)
-#   unionVectors = vectors[[1]]
-#   for(i in 1:length(vectors)){
-#     unionVectors = union(unionVectors, vectors[[i]])
-#   }
-#   return(unionVectors)
-# }
-
-
